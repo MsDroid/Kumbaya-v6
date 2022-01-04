20d4b68e9d86f1912303efb4be6ca337294a314a
MsDroid <63984801+MsDroid@users.noreply.github.com>
2022-01-04 18:17:11 +0530

    Update urlConfig.js

--- urlConfig.js
+++ urlConfig.js
@@ -1,5 +1,5 @@
 // site url
 // export const base_url = "http://localhost:3000/";
-export const base_url = "https://kumbaya-v6-cqemy3s0i-msdroid.vercel.app/"
+export const base_url = "https://kumbaya-v6-cqemy3s0i-msdroid.vercel.app/";
 
 
